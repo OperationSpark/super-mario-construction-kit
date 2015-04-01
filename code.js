f94c66578bb7ca0fb43b63e87771271a3efa5efe
Joel Carranza <joel.carranza@gmail.com>
2015-04-01 17:45:59 -0500

    initial code for stage

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 'use strict';
 
-rect(15, 15, 50, 50);
+// draw the background
+background(127, 131, 231);
+
+// draw the floor
+var yGround = 500;
+fill(156,102,17); // brown
+rect(0,yGround,600,600-yGround);
+
+// draw a pipe coming out of floor
+fill(141,199,17); // green
+rect(400,400,100,50);
+rect(410,450,80,50);
+
+// box
+fill(216,216,216);
+rect(0,0,100,100);
+line(0,0,100,100);
+line(100,0,0,100);
+
