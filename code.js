57912c32309084c550adb7dd6774b99d5841fdbc
Joel Carranza <joel.carranza@gmail.com>
2015-04-02 12:42:38 -0500

    comment code.js for readability and easy modification

--- code.js
+++ code.js
@@ -1,20 +1,25 @@
 /* global background,fill,rect,line */
 'use strict';
 
-// draw the background
+// draws the background in blue
 background(127, 131, 231);
 
-// draw the floor
+// draws a brown floor
 var yGround = 500;
-fill(156,102,17); // brown
+fill(156,102,17);
 rect(0,yGround,600,600-yGround);
 
-// draw a pipe coming out of floor
-fill(141,199,17); // green
-rect(400,400,100,50);
-rect(410,450,80,50);
+// draw a green pipe coming out of floor
+var pipeHeight = 150;
+fill(141,199,17); 
+rect(400+10,yGround-pipeHeight,80,pipeHeight);
+rect(400,yGround-pipeHeight,100,50);
 
-// box
+/* ============================================= 
+   Start Here! 
+   ============================================= */
+
+// draws a box with an X in it
 fill(216,216,216);
 rect(0,0,100,100);
 line(0,0,100,100);
